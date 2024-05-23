a935c8bfea02ae820bab27bed3772fed4239bf1d
scifi-holic <scifi.holic@gmail.com>
2024-05-24 03:39:34 +0900

    update

--- linear_regression/src/LinearRegression.ts
+++ linear_regression/src/LinearRegression.ts
@@ -1,24 +1,25 @@
-import { ZkProgram, Int64, Provable } from 'o1js';
+// import { ZkProgram, Int64, Provable } from 'o1js';
 
-const LinearRegression = ZkProgram({
-    name: 'LinearRegression',
-    publicOutput: Int64,
-  methods: {
-    predict: {
-      privateInputs: [Provable.Array(Int64, 2)],
+// const LinearRegression = ZkProgram({
+//     name: 'LinearRegression',
+//     publicOutput: Int64,
+//   methods: {
+//     predict: {
+//       privateInputs: [Provable.Array(Int64, 2)],
 
-      method(input: Int64[]): Int64 {
-        const coefficients = [Int64.from(5), Int64.from(5)];
-        const intercept = Int64.from(0);
-        let dotProduct = Int64.from(0);
+//       method(input: Int64[]): Int64 {
+//         const coefficients = [Int64.from(5), Int64.from(5)];
+//         const intercept = Int64.from(0);
+//         let dotProduct = Int64.from(0);
 
-        for (let i = 0; i < coefficients.length; i++) {
-          dotProduct = dotProduct.add(coefficients[i].mul(input[i]));
-        }
+//         for (let i = 0; i < coefficients.length; i++) {
+//           dotProduct = dotProduct.add(coefficients[i].mul(input[i]));
+//         }
 
-        const z = dotProduct.div(10).add(intercept); 
-        return z;
-    },
-  },
-},
-});
+//         const z = dotProduct.div(10).add(intercept); 
+//         return z;
+//     },
+//   },
+// },
+// });
+
