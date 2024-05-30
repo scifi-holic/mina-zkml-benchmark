e4d0ad2068889fe4c29cabf67aba1ba46c89603a
scifi-holic <scifi.holic@gmail.com>
2024-05-30 23:38:29 +0900

    WIP: ZKP

--- linear_regression/src/LinearRegression.ts
+++ linear_regression/src/LinearRegression.ts
@@ -23,47 +23,31 @@
 // },
 // });
 
-import { Field, SelfProof, ZkProgram, verify } from 'o1js';
+import { Field, Int64, Provable, ZkProgram, verify } from 'o1js';
 
 const LinearRegression = ZkProgram({
   name: 'linear-regression',
-  publicInput: Field,
+  publicInput: Int64,
+  publicOutput: Int64,
 
   methods: {
-    init: {
-      privateInputs: [],
 
-      async method(state: Field) {
-        state.assertEquals(Field(0));
-      },
-    },
     predict: {
       privateInputs: [Provable.Array(Int64, 2)],
 
-      method(input: Int64[]): Int64 {
-        const coefficients = [Int64.from(5), Int64.from(5)];
+      async method(
+        input: Int64
+      ): Promise<Int64> {
+        const coeff = [Int64.from(5), Int64.from(5)];
         const intercept = Int64.from(0);
         let dotProduct = Int64.from(0);
-
-        for (let i = 0; i < coefficients.length; i++) {
-          dotProduct = dotProduct.add(coefficients[i].mul(input[i]));
+        for (let i = 0; i< coeff.length; i++) {
+          dotProduct = dotProduct.add(coeff[i].mul(input));
         }
-
-        const z = dotProduct.div(10).add(intercept); 
+        const z = dotProduct.div(10).add(intercept);
         return z;
+      },
     },
-    // predict: {
-    //   privateInputs: [SelfProof, Field],
-
-    //   async method(
-    //     newState: Field,
-    //     earlierProof: SelfProof<Field, void>,
-    //     numberToAdd: Field
-    //   ) {
-    //     earlierProof.verify();
-    //     newState.assertEquals(earlierProof.publicInput.add(numberToAdd));
-    //   },
-    // },
 
   },
 });
@@ -74,18 +58,21 @@ async function main() {
   const { verificationKey } = await LinearRegression.compile();
 
   console.log('making proof 0');
+  let input = [Int64.from(25)];
+  const res0 = await LinearRegression.predict(input);
+  console.log('res 0: ', res0);
 
-  const proof0 = await LinearRegression.init(Field(0));
+  // const proof0 = await LinearRegression.init(Field(0));
 
-  console.log('making proof 2');
+  // console.log('making proof 2');
 
-  const proof2 = await LinearRegression.predict(Field(4), proof0, Field(4);
+  // const proof2 = await LinearRegression.predict(Field(4), proof0, Field(4);
 
-  console.log('verifying proof 2');
-  console.log('proof 2 data', proof2.publicInput.toString());
+  // console.log('verifying proof 2');
+  // console.log('proof 2 data', proof2.publicInput.toString());
 
-  const ok = await verify(proof2.toJSON(), verificationKey);
-  console.log('ok', ok);
+  // const ok = await verify(proof2.toJSON(), verificationKey);
+  // console.log('ok', ok);
 }
 
 main();
